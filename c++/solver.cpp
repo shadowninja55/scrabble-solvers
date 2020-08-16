9601bd51ee3663a5863deecc97876c068f440564
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-08-16 16:03:21 -0400

    Added main files / directories

---
+++ c++/solver.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <map>
+using namespace std;
+
+int count(string s, char c) {
+    int count = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == c) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+bool valid(string word, string letters) {
+    char c;
+    for (int x = 0; x < word.size(); x++) {
+        c = word[x];
+        if (count(word, c) > count(letters, c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> generateWords(string letters) {
+    vector<string> words;
+    string line;
+    ifstream file;
+    file.open("dictionary.txt");
+
+    while (getline(file, line)) {
+        if (valid(line, letters)) {
+            words.push_back(line);
+        }
+    }
+    file.close();
+
+    return words;
+}
+
+int score(string word) {
+    map<char, int> scores;
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    int values[26] = {
+        1, 3, 3, 2, 1, 4,
+        2, 4, 1, 8, 5, 1,
+        3, 1, 1, 3, 10, 1,
+        1, 1, 1, 4, 4, 8,
+        4, 10
+    };
+    for (int i = 0; i < 26; i++) {
+        scores[letters[i]] = values[i];
+    }
+
+    int total = 0;
+    for (int i = 0; i < word.size(); i++) {
+        total += scores[word[i]];
+    }
+
+    return total;
+}
+
+vector<string> sorted(vector<string> words) {
+    sort(words.begin(), words.end(), [](auto &first, auto &second) {
+        return score(first) < score(second);
+    });
+    return words;
+}
+
+string display(vector<string> words) {
+    string result;
+    int index;
+
+    for (int i = words.size(); i > 0; i--) {
+        index = words.size() - i;
+        result += (to_string(i)  + ". " + words[index] + "\n");
+    }
+    return result;
+}
+
+int main() {
+    string letters;
+    cout << "Letters: ";
+    cin >> letters;
+    vector<string> words = generateWords(letters);
+    words = sorted(words);
+    string result = display(words);
+    cout << result;
+
+    system("pause");
+	return 0;
+}
---
+++ cython/source.pyx
@@ -0,0 +1,50 @@
+import json
+
+def load_dictionary():
+    with open('dictionary.txt') as f:
+        return f.read().split('\n')
+
+def generate_words(letters, dictionary):
+    cdef str word
+    cdef str letter
+    new_words = []
+
+    for word in dictionary:
+        if all(word.count(letter) <= letters.count(letter) for letter in word):
+            new_words.append(word)
+
+    return new_words
+
+def score(str word):
+    scores = {
+         "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
+         "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
+         "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
+         "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
+         "q": 10, "r": 1 , "s": 1 , "t": 1 ,
+         "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
+         "y": 4 , "z": 10
+    }
+
+    cdef str c
+    return sum(scores[c] for c in word)
+
+def sort(words, letters, dictionary):
+    return sorted(words, key=score, reverse=True)
+
+def display(words):
+    cdef int c
+    cdef str word
+    words = list(reversed([f'{c}. {word}' for c, word in enumerate(words, 1)]))
+    return '\n'.join(words)
+
+def main():
+    dictionary = load_dictionary()
+    letters = input('Letters: ')
+    words = generate_words(letters, dictionary)
+    words = sort(words, letters, dictionary)
+    words = display(words)
+    print(words)
+    return 0
+
+main()
