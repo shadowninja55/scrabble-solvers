2a077998cf83ecaf4843742b7a49da9b97d8e9cb
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-08-18 13:28:18 -0400

    Update solver.js

--- node.js/solver.js
+++ node.js/solver.js
@@ -31,17 +31,17 @@ function generateWords(letters) {
     return words;
 }
 
-function score(word) {
-    scores = {
-        "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
-        "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
-        "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
-        "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
-        "q": 10, "r": 1 , "s": 1 , "t": 1 ,
-        "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
-        "y": 4 , "z": 10
-    };
+const scores = {
+    "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
+    "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
+    "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
+    "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
+    "q": 10, "r": 1 , "s": 1 , "t": 1 ,
+    "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
+    "y": 4 , "z": 10
+};
 
+function score(word) {
     let score = 0;
     for (let i = 0; i < word.length; i++) {
         score += scores[word[i]];
@@ -59,7 +59,7 @@ function display(words) {
 
     for (let i = words.length; i > 0; i--) {
         let index = words.length - i;
-        result += (i  + ". " + words[index] + "\n");
+        result += (i + ". " + words[index] + "\n");
     }
     return result;
 }
