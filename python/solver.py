fc274448c346f18481a2fd3a11d0833cd4c54269
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-08-18 13:26:03 -0400

    Add files via upload

--- python/solver.py
+++ python/solver.py
@@ -1,41 +1,44 @@
 import json
 
 def generate_words(letters):
-    new_words = []
+    words = []
     with open('dictionary.txt') as f:
-        dictionary = f.read().split('\n')
+        for word in f.read().split('\n'):
+            if all(word.count(letter) <= letters.count(letter) for letter in word):
+                words.append(word)
 
-    for word in dictionary:
-        if all(word.count(letter) <= letters.count(letter) for letter in word):
-            new_words.append(word)
+    return words
 
-    return new_words
+scores = {
+     "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
+     "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
+     "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
+     "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
+     "q": 10, "r": 1 , "s": 1 , "t": 1 ,
+     "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
+     "y": 4 , "z": 10
+}
 
 def score(word):
-    scores = {
-         "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
-         "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
-         "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
-         "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
-         "q": 10, "r": 1 , "s": 1 , "t": 1 ,
-         "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
-         "y": 4 , "z": 10
-    }
-
     return sum(scores[c] for c in word)
 
 def sort(words, letters):
-    return sorted(words, key=score, reverse=True)
+    return sorted(words, key=score)
 
 def display(words):
-    words = reversed([f'{c}. {word}' for c, word in enumerate(words, 1)])
-    return '\n'.join(words)
+    result = ''
+
+    for i in reversed(range(1, len(words) + 1)):
+        index = len(words) - i
+        result += f'{i}. {words[index]}\n'
+
+    return result
 
 def main():
-    letters = input('Letters: ')
+    letters = 'abcdefg' # input('Letters: ')
     words = generate_words(letters)
     words = sort(words, letters)
-    words = display(words)
-    print(words)
+    result = display(words)
+    print(result)
 
 main()
