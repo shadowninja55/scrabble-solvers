00935c9d2f2379218c1df3f7769ec7be0ab52e01
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-08-18 13:29:12 -0400

    Update source.pyx

--- cython/source.pyx
+++ cython/source.pyx
@@ -43,7 +43,7 @@ def display(words):
     return result
 
 def main():
-    letters = 'abcdefg' # input('Letters: ')
+    letters = input('Letters: ')
     words = generate_words(letters)
     words = sort(words, letters)
     result = display(words)
