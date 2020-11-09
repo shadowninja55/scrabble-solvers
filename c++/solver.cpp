ddb5e9f006218392bc4f11404e20c57ce64eb3fe
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-11-09 10:15:42 -0500

    Update solver.cpp

--- c++/solver.cpp
+++ c++/solver.cpp
@@ -5,9 +5,11 @@
 #include <fstream>
 using namespace std;
 
-bool valid(string word, string letters) {
-    for (int x = 0; x < word.size(); x++) {
-        char c = word[x];
+string letters = "abcdefg";
+vector<string> words;
+
+bool valid(string word) {
+    for (auto c : word) {
         if (count(word.begin(), word.end(), c) > count(letters.begin(), letters.end(), c)) {
             return false;
         }
@@ -15,75 +17,54 @@ bool valid(string word, string letters) {
     return true;
 }
 
-vector<string> generateWords(string letters) {
-    vector<string> words;
-    string line;
-    ifstream file;
-    file.open("dictionary.txt");
+void generateWords() {
+    string word;
+    ifstream file("dictionary.txt");
 
-    while (getline(file, line)) {
-        if (valid(line, letters)) {
-            words.push_back(line);
+    while (getline(file, word)) {
+        if (valid(word)) {
+            words.push_back(word);
         }
     }
-    file.close();
-
-    return words;
 }
 
-map<char, int> scores;
-
-void constructScores() {
-    string letters = "abcdefghijklmnopqrstuvwxyz";
-    int values[26] = {
-        1, 3, 3, 2, 1, 4,
-        2, 4, 1, 8, 5, 1,
-        3, 1, 1, 3, 10, 1,
-        1, 1, 1, 4, 4, 8,
-        4, 10
-    };
-    for (int i = 0; i < 26; i++) {
-        scores[letters[i]] = values[i];
-    }
-}
+map<char, int> scores = {
+    {'a', 1}, {'b', 3}, {'c', 3}, {'d', 2},
+    {'e', 1}, {'f', 4}, {'g', 2}, {'h', 4},
+    {'i', 1}, {'j', 8}, {'k', 5}, {'l', 1},
+    {'m', 3}, {'n', 1}, {'o', 1}, {'p', 3},
+    {'q', 10},{'r', 1}, {'s', 1}, {'t', 1},
+    {'u', 1}, {'v', 4}, {'w', 4}, {'x', 8},
+    {'y', 4}, {'z', 10}
+};
 
 int score(string word) {
     int total = 0;
-    for (int i = 0; i < word.size(); i++) {
-        total += scores[word[i]];
+
+    for (auto c : word) {
+        total += scores[c];
     }
 
     return total;
 }
 
-vector<string> sorted(vector<string> words) {
-    sort(words.begin(), words.end(), [](auto &first, auto &second) {
-        return score(first) < score(second);
+void sortWords() {
+    sort(words.begin(), words.end(), [](auto a, auto b) {
+        return score(a) < score(b);
     });
-    return words;
 }
 
-string display(vector<string> words) {
-    string result;
-    int index;
+void displayWords() {
+    int length = words.size();
 
-    for (int i = words.size(); i > 0; i--) {
-        index = words.size() - i;
-        result += (to_string(i)  + ". " + words[index] + "\n");
+    for (int i = length; i > 0; i--) {
+        int index = length - i;
+        cout << i << ". " << words[index] << endl;
     }
-    return result;
 }
 
 int main() {
-    string letters;
-    cout << "Letters: ";
-    cin >> letters;
-    vector<string> words = generateWords(letters);
-    constructScores();
-    words = sorted(words);
-    string result = display(words);
-    cout << result;
-
-    system("pause");
-    return 0;
+    generateWords();
+    sortWords();
+    displayWords();
 }
