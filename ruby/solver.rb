ce3a8b1b86a8a15eae0aef8fddad60fca1f9295a
shadowninja55 <49539636+hyper-blade@users.noreply.github.com>
2020-11-10 10:48:12 -0500

    Update solver.rb

--- ruby/solver.rb
+++ ruby/solver.rb
@@ -1,61 +1,62 @@
-def valid word, letters
-    for c in word.split ''
-      if word.count(c) > letters.count(c)
-          return false
-      end
+$words = []
+$letters = "abcdefg"
+
+def valid(word)
+    for c in word.split('')
+        if word.count(c) > $letters.count(c)
+            return false
+        end
     end
+
     return true
 end
 
-def generate_words letters
-    words = []
+def generate_words()
     f = File.open("dictionary.txt")
-    contents = f.read
-    dictionary = contents.split "\n"
-    for word in dictionary
-        if valid(word, letters)
-            words.append word
+
+    for word in f.read().split("\n")
+        if valid(word)
+            $words.append(word)
         end
     end
-    return words
 end
 
-def score word
-    scores = {
-        "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
-        "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
-        "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
-        "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
-        "q": 10, "r": 1 , "s": 1 , "t": 1 ,
-        "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
-        "y": 4 , "z": 10
-    }
+$scores = {
+    "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
+    "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
+    "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
+    "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
+    "q": 10, "r": 1 , "s": 1 , "t": 1 ,
+    "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
+    "y": 4 , "z": 10
+}
+
+def score(word)
     score = 0
+
     for c in word.split('')
-        score += scores[:"#{c}"]
+        score += $scores[:"#{c}"]
     end
+
     return score
 end
 
-def sort_words words
-    words.sort_by { |w| score w }
+def sort_words()
+    $words = $words.sort_by {|w| score(w)}
 end
 
-def display words
-    result = ""
-    wlen = words.length
-    words.each_with_index do |word,i|
-        result += "#{wlen-i}. #{word}\n"
+def display_words()
+    length = $words.length
+
+    $words.each_with_index do |word, i|
+        puts "#{length - i}. #{word}"
     end
-    return result
 end
 
 def main
-    print "Letters: "
-    letters = gets
-    words = generate_words letters
-    words = sort_words words
-    puts display words
+    generate_words()
+    sort_words()
+    display_words()
 end
 
-main
+main()
