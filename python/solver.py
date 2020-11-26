5467c60bbd35d793fadcec498330f12abef8a6b8
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-11-26 16:36:40 -0500

    Update solver.py

--- python/solver.py
+++ python/solver.py
@@ -1,35 +1,33 @@
-import json
-
-def generate_words(letters):
-    words = []
+def generate_words():
     with open('dictionary.txt') as f:
         for word in f.read().split('\n'):
             if all(word.count(letter) <= letters.count(letter) for letter in word):
                 words.append(word)
 
-    return words
-
 scores = {
-     "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
-     "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
-     "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
-     "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
-     "q": 10, "r": 1 , "s": 1 , "t": 1 ,
-     "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
-     "y": 4 , "z": 10
+    "a": 1 , "b": 3 , "c": 3 , "d": 2 ,
+    "e": 1 , "f": 4 , "g": 2 , "h": 4 ,
+    "i": 1 , "j": 8 , "k": 5 , "l": 1 ,
+    "m": 3 , "n": 1 , "o": 1 , "p": 3 ,
+    "q": 10, "r": 1 , "s": 1 , "t": 1 ,
+    "u": 1 , "v": 4 , "w": 4 , "x": 8 ,
+    "y": 4 , "z": 10
 }
 
 def score(word):
     return sum(scores[c] for c in word)
 
-def display(words):
-    for i, word in enumerate(reversed(words), 1):
-        print(f"{i}. {words[index]}")
+def display_words():
+    length = len(words)
+    for i in reversed(range(1, len(words) + 1)):
+        print(f'{i}. {length - i}')
+
+letters = "abcdefg"
+words = []
 
 def main():
-    letters = input('Letters: ')
-    words = generate_words(letters)
+    generate_words()
     words.sort(key=score)
-    display(words)
+    display_words()
 
 main()
