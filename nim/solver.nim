adedb101e3eaa91f96505f683f183d768820f712
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-02 12:31:54 -0500

    Update solver.nim

--- nim/solver.nim
+++ nim/solver.nim
@@ -1,22 +1,22 @@
 import strutils
 import tables
 import algorithm
+import sugar
 import strformat
 
-const letters = "abcdefg"
-var words: seq[string] = @[]
+let letters = "abcdefg"
 
 proc isValid(word: string): bool =
     for letter in word:
         if word.count(letter) > letters.count(letter):
             return false
-    
+
     return true
 
-proc loadValidWords() =
+proc loadValidWords(): seq[string] =
     for line in lines("dictionary.txt"):
-        if isValid(line):
-            words.add(line)
+        if isValid(line): 
+            result.add(line)
 
 const scores = {
  'a': 1, 'b': 3, 'c': 3, 'd': 2,
@@ -26,25 +26,21 @@ const scores = {
  'q': 10, 'r': 1, 's': 1, 't': 1,
  'u': 1, 'v': 4, 'w': 4, 'x': 8,
  'y': 4, 'z': 10
-}.toTable
+}.toTable()
 
 proc getScore(word: string): int =
-    var total = 0
+    for letter in word: 
+        result += scores[letter]
 
-    for letter in word:
-        total += scores[letter]
-    
-    return total
+proc sortWords(words: var seq[string]) =
+    words.sort((a, b) => (
+        if getScore(a) < getScore(b): -1 else: 1
+    ))
 
-proc sortWords() =
-    words.sort(proc (a, b: string): int = (if getScore(a) < getScore(b): -1 else: 1))
-
-proc displayWords() =
-    let length = len(words)
-
-    for i in countdown(length, 1):
+proc displayWords(words: seq[string]) =
+    for i in countdown(words.len, 1):
         echo &"{i}. {words[^i]}"
 
-loadValidWords()
-sortWords()
-displayWords()
+var words = loadValidWords()
+sortWords(words)
+displayWords(words)
