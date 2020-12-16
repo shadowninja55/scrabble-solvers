03f5965132c8206ee1e7e83d14b3a4b5cd5bafbd
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-16 17:33:47 -0500

    Update solver.nim

--- nim/solver.nim
+++ nim/solver.nim
@@ -1,38 +1,34 @@
-import strutils
-import tables
-import algorithm
-import sugar
-import strformat
+import algorithm, strformat, strutils, sugar, tables
 
 let letters = "abcdefg"
 
 proc isValid(word: string): bool =
+    result = true
+
     for letter in word:
         if word.count(letter) > letters.count(letter):
             return false
 
-    return true
-
 proc loadValidWords(): seq[string] =
-    for line in lines("dictionary.txt"):
-        if isValid(line): 
-            result.add(line)
-
-const scores = {
- 'a': 1, 'b': 3, 'c': 3, 'd': 2,
- 'e': 1, 'f': 4, 'g': 2, 'h': 4,
- 'i': 1, 'j': 8, 'k': 5, 'l': 1,
- 'm': 3, 'n': 1, 'o': 1, 'p': 3,
- 'q': 10, 'r': 1, 's': 1, 't': 1,
- 'u': 1, 'v': 4, 'w': 4, 'x': 8,
- 'y': 4, 'z': 10
-}.toTable()
+    for line in lines "dictionary.txt":
+        if line.isValid:
+            result.add line
 
 proc getScore(word: string): int =
-    for letter in word: 
+    const scores = {
+      'a': 1, 'b': 3, 'c': 3, 'd': 2,
+      'e': 1, 'f': 4, 'g': 2, 'h': 4,
+      'i': 1, 'j': 8, 'k': 5, 'l': 1,
+      'm': 3, 'n': 1, 'o': 1, 'p': 3,
+      'q': 10, 'r': 1, 's': 1, 't': 1,
+      'u': 1, 'v': 4, 'w': 4, 'x': 8,
+      'y': 4, 'z': 10
+    }.toTable
+
+    for letter in word:
         result += scores[letter]
 
-proc sortWords(words: var seq[string]) =
+func sortWords(words: var seq[string]) =
     words.sort((a, b) => (
         if getScore(a) < getScore(b): -1 else: 1
     ))
@@ -42,5 +38,5 @@ proc displayWords(words: seq[string]) =
         echo &"{i}. {words[^i]}"
 
 var words = loadValidWords()
-sortWords(words)
-displayWords(words)
+sortWords words
+displayWords words
