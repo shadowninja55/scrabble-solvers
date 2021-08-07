2e2a08763957f5b3a2f2da4cdde605c1b4e8b626
shadowninja55 <shadowninja1050@gmail.com>
2021-08-07 14:29:07 -0400

    optimize haskell (time complexity, runs slower)

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,32 +1,34 @@
 module Main where
 
-import Data.Char (ord)
-import Data.List (sortOn)
+import qualified Data.Char as C
 import Text.Printf (printf)
-import Data.Vector (fromList, (!))
+import qualified Data.Vector as V 
+import qualified Data.List as L
+import qualified Data.MultiSet as M
 
-isValid :: String -> String -> Bool
-isValid word letters = all f word
-  where 
-  f letter = count letter word <= count letter letters
-  count char = length . filter (== char)
-
-score :: String -> Int 
+score :: String -> Int
 score = sum . fmap f
   where 
-  f letter = scores ! (ord letter - 97)
-  scores = fromList [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 
-    1, 11, 4, 4, 8, 4, 10]
+    f letter = scores V.! (C.ord letter - 97)
+    scores = V.fromList [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 
+      1, 11, 4, 4, 8, 4, 10]
 
-prettyPrint :: [String] -> String
-prettyPrint = unlines . reverse . zipWith f [(1 :: Int)..]
-  where f idx word = printf "%d. %s" idx word
+render :: [String] -> String
+render = unlines . reverse . zipWith f [(1 :: Int)..]
+  where 
+    f = printf "%d. %s" 
 
-process :: String -> String -> String
-process letters = prettyPrint . sortOn (negate . score) . filter valid . lines
-  where valid = (`isValid` letters)
+process :: M.MultiSet Char -> String -> String
+process letters content = 
+    render sorted
+  where
+    sorted = L.sortOn (negate . score) (V.toList validWords)
+    validWords = fmap (wordList V.!) validWordIndices
+    validWordIndices = V.findIndices (`M.isSubsetOf` letters) charSetList
+    charSetList = M.fromList <$> wordList
+    wordList = V.fromList . lines $ content
 
 main :: IO ()
 main = do
   content <- readFile "dictionary.txt"
-  putStr . process "abcdefg" $ content
+  putStr $ process (M.fromList "abcdefg") content
